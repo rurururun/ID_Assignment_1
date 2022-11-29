37d7bdc008fed4181c86941990bcedea61d7d6cb
Chua Guo Heng <guohengchua@gmail.com>
2022-11-29 20:13:36 +0800

    Added in showing articles based on button clicked

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+const aboutMe = `
+    <h1>About Me</h1>
+    <p>Testing</p>
+`;
+
 var buttonClicked = null;
+
+let newDiv = null;
 
 function choice(x){
     if (buttonClicked != null){
@@ -8,4 +15,21 @@ function choice(x){
     buttonClicked = x;
     buttonClicked.style.backgroundColor = "lightblue";
     buttonClicked.style.borderColor = "lightskyblue";
+
+    if (newDiv != null){
+        document.querySelector("div div article").remove();
+        newDiv = null;
+        if (buttonClicked.getAttribute("id") == "test"){
+            newDiv = document.createElement("article");
+            newDiv.innerHTML = aboutMe;
+            document.querySelector("div div").append(newDiv);
+        }
+    }
+    else{
+        if (buttonClicked.getAttribute("id") == "test"){
+            newDiv = document.createElement("article");
+            newDiv.innerHTML = aboutMe;
+            document.querySelector("div div").append(newDiv);
+        }
+    }
 }
